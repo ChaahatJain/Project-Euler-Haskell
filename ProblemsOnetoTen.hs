31a67b025938d1233074358f3295d68b34e6fdac
Chaahat <u6398806@anu.edu.au>
2017-10-25 21:29:54 +1100

    Test

--- ProblemsOnetoTen.hs
+++ ProblemsOnetoTen.hs
@@ -2,10 +2,15 @@ module ProblemsOnetoTen where
 
 import Data.List (union,(\\))
 
-multiple3or5 :: [Int]
-multiple3or5 = ([i | i <- [0 .. 1000], i `mod` 3 == 0] `union` [i |i <- [0 .. 1000], i `mod` 5 == 0])
+multiple3or5 :: Int
+multiple3or5 = sum ([i | i <- [0 .. 1000], i `mod` 3 == 0] `union` [i |i <- [0 .. 999], i `mod` 5 == 0])
+-------------------------
+fibList :: (Int,Int) -> [Int]
+fibList (x,y) = x : fibList (y,x+y)
 
+getList (x,y) = sum (filter (even )(takeWhile (< 4000000) (fibList (x,y))))
 
+-------------------------
 isPrime :: Integer -> Bool
 isPrime x = null $ filter (\i -> x `mod` i == 0) [2 .. (x - 1)]
 
