175b8606c89fb23c3bf4be164f611759d3e2501c
Chaahat <u6398806@anu.edu.au>
2017-10-25 22:13:50 +1100

    Test

--- ProblemsOnetoTen.hs
+++ ProblemsOnetoTen.hs
@@ -1,6 +1,6 @@
 module ProblemsOnetoTen where
 
-import Data.List (union,(\\))
+import Data.List
 
 multiple3or5 :: Int
 multiple3or5 = sum ([i | i <- [0 .. 1000], i `mod` 3 == 0] `union` [i |i <- [0 .. 999], i `mod` 5 == 0])
@@ -24,3 +24,25 @@ fastReverse :: [a] -> [a]
 fastReverse list = case list of
     [] -> []
     x:xs -> foldl (\list' a -> a:list') [] list
+------------------------
+isPalindrome :: Int -> Bool
+isPalindrome int = list == reverse list
+    where
+    list = show int
+
+palList :: Int
+palList =  head $ fastReverse $ sort [a*b | a <- [999,998..100], b <- [999,998..100], (isPalindrome (a*b))]
+------------------------
+minDiv20 :: Int
+minDiv20 = foldl lcm 1 [1..20]
+-----------------------
+sumSquareDiff :: Int -> Int
+sumSquareDiff n = sumSquare n - squareSum n
+
+sumSquare :: Int -> Int
+sumSquare n = sum [a^2 | a <- [1..n]]
+
+squareSum :: Int -> Int
+squareSum n = (sum [a | a <- [1..n]])^2
+-----------------------
+list = head $[(a,b,c) | a<-[100..500],b<-[100..500],c <- [100..500], a+b+c ==1000,a^2 + b^2 == c^2]
